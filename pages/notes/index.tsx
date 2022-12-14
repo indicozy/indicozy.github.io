87aa327935adc46eaffe9f9843831f293222175c
indicozy <berkutkarlibai@gmail.com>
2022-12-14 13:33:18 +0600

    init

--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -19,9 +19,64 @@ export const getStaticProps = async () => {
 }
 
 export default function NotionDomainPage(props) {
+  const notes = Object.values(props.recordMap.block).map((block: any, i) => (
+    <>
+      {block.value &&
+      block.value.properties &&
+      block.value.type &&
+      block.value.type === 'page' ? (
+        <div key={i} className='p-4'>
+          {block.value.properties.title ? (
+            <>
+              <h2 className='text-xl'>{block.value.properties.title[0][0]}</h2>
+              <p>
+                {block.value.properties.title[0][0]
+                  .toLowerCase()
+                  .replace(/[^A-Za-z0-9 ]/g, '')
+                  .replaceAll(' ', '-')}
+              </p>
+            </>
+          ) : (
+            <></>
+          )}
+          {block.value.id ? <p>{block.value.id}</p> : <></>}
+          {block.value.properties['Y]q}'] ? (
+            <p>{block.value.properties['Y]q}'][0][0]}</p>
+          ) : (
+            <></>
+          )}
+          {block.value.properties['kKTk'] ? (
+            <p>{block.value.properties['kKTk'][0][0]}</p>
+          ) : (
+            <></>
+          )}
+          {block.value.properties['jRIm'] ? (
+            <p>{block.value.properties['jRIm'][0][0]}</p>
+          ) : (
+            <></>
+          )}
+          {block.value.properties['ea=E'] ? (
+            <p>
+              {JSON.stringify(block.value.properties['ea=E'][0][0].split(','))}
+            </p>
+          ) : (
+            <></>
+          )}
+          {block.value.properties['cKLX'] ? (
+            <p>{block.value.properties['cKLX'][0][0]}</p>
+          ) : (
+            <></>
+          )}
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  ))
   return (
     <>
-      <NotionPage {...props} />
+      {notes}
+      {/* <NotionPage {...props} /> */}
     </>
   )
 }
--- site.config.ts
+++ site.config.ts
@@ -9,8 +9,8 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: 'Personal Portfolio page',
+  domain: 'indicozy.vercel.app',
   author: 'indicozy',
 
   // open graph metadata (optional)
