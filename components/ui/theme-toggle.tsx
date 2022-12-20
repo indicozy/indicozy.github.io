e47fb6b5e6c4dd86979f1af1d9664294b70d7ad9
indicozy <berkutkarlibai@gmail.com>
2022-12-21 00:07:28 +0600

    init

--- components/ui/theme-toggle.tsx
+++ components/ui/theme-toggle.tsx
@@ -1,13 +1,8 @@
-import { useEffect, useState } from 'react'
-
 import { IconMoon, IconSun } from '@tabler/icons'
 import { useTheme } from 'next-themes'
 
 const ThemeToggler = () => {
   const { theme, setTheme } = useTheme()
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => setMounted(true), [])
-  if (!mounted) return null
   return (
     <button
       className='flex justify-center items-center w-8 h-8 rounded-lg transition-all duration-300 focus:outline-none'
