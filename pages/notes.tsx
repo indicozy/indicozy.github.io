550058f939a3569b81d99ee6b785dac51742abf4
indicozy <berkutkarlibai@gmail.com>
2022-12-22 14:49:22 +0600

    init

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -11,7 +11,7 @@ export const getStaticProps = async () => {
   try {
     const props = await resolveNotionPage(domain)
 
-    return { props, revalidate: 3600 }
+    return { props, revalidate: 600 }
   } catch (err) {
     console.error('page error', domain, err)
 
