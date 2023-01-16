280262bf8d7192f2cb23bb3306c603785262092c
indicozy <berkutkarlibai@gmail.com>
2023-01-16 13:59:38 +0600

    test

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,10 +10,15 @@ const SkillsAll: FC = () => {
   const { t } = useTranslation()
   return (
     <>
+      <h2 className='mb-6 text-6xl font-bold font-domain'>
+        {t('skills.header')}
+      </h2>
       {Object.entries(skills).map(([key, value], index) => {
         return (
-          <div key={index}>
-            <div>{value.locales[t('lang')] || value.locales['en']}</div>
+          <div key={index} className='mb-4'>
+            <div className='mb-2 text-2xl'>
+              {value.locales[t('lang')] || value.locales['en']}
+            </div>
             <div>
               {value.skills.map((skill, skillIndex) => (
                 <Badge key={skillIndex} icon={skill.icon}>
