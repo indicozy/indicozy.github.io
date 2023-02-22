cdaac1469cd4d1b343d922438e48570fe79d0902
indicozy <berkutkarlibai@gmail.com>
2023-02-22 14:47:19 +0600

    temporarily removed group channels

--- components/ReactPdf.tsx
+++ components/ReactPdf.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { FC, ReactNode } from 'react'
 
 import {
   Document,
+  Font,
   PDFViewer,
   Page,
   StyleSheet,
@@ -9,72 +10,144 @@ import {
   View
 } from '@react-pdf/renderer'
 
-// transform: 'rotate(90deg)'
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    // flexDirection: 'row',
-    backgroundColor: '#ece7e1'
-  },
-  section: {
-    // margin: 10,
-    // padding: 10,
-    flexGrow: 1
-  }
+Font.register({
+  family: 'Atyp Regular Variable',
+  fonts: [
+    { src: '/fonts/atyp/variable.ttf' } // font-style: normal, font-weight: normal
+  ]
 })
+
+const ViewSection: FC<{ children: ReactNode; sidebar: string }> = ({
+  children,
+  sidebar
+}) => {
+  return (
+    <View style={{ minHeight: 70, backgroundColor: 'red' }}>
+      <View>
+        <Text
+          style={{
+            transform: 'rotate(90deg) translate(128px, 110px)',
+            fontSize: 8,
+            textTransform: 'uppercase',
+            fontWeight: 'bold',
+            fontFamily: 'Atyp Regular Variable'
+          }}
+        >
+          {sidebar}
+        </Text>
+      </View>
+      <View
+        style={{ marginLeft: 30, display: 'flex', flexDirection: 'column' }}
+      >
+        {children}
+      </View>
+    </View>
+  )
+}
+
+const TextSection: FC<{ children: ReactNode }> = ({ children }) => {
+  return <Text style={{ fontSize: 8 }}>{children}</Text>
+}
 
 // Create Document Component
 const MyDocument = ({ t }) => {
   return (
     <Document>
-      <Page size='A4' style={styles.page}>
-        <View style={{ flexDirection: 'row' }}>
-          <View style={styles.section}>
-            <Text style={{ fontSize: 30 }}>{t('name')}</Text>
+      <Page
+        size='A4'
+        style={{
+          backgroundColor: '#ece7e1'
+        }}
+      >
+        <View>
+          <View style={{ height: 0 }}>
+            <Text
+              style={{
+                transform: 'rotate(90deg) translate(320px, 270px)',
+                fontSize: 10
+              }}
+            >
+              2023
+            </Text>
           </View>
-          <View style={styles.section}>
-            <Text>Section #2</Text>
-          </View>
-        </View>
-        <View style={{ display: 'flex', flexDirection: 'row' }}>
-          <View style={{ flexBasis: 1, flexGrow: 1, display: 'flex' }}>
-            <View>
-              <Text
+          <View
+            style={{
+              marginLeft: 40,
+              marginTop: 40,
+              marginBottom: 40,
+              marginRight: 40
+            }}
+          >
+            <View
+              style={{
+                flexDirection: 'row',
+                display: 'flex',
+                alignItems: 'flex-end'
+              }}
+            >
+              <View
                 style={{
-                  transform: 'rotate(90deg) translate(160px, 130px)',
-                  fontSize: 12
+                  flexBasis: 1,
+                  flexGrow: 1
                 }}
               >
-                EDUCATION
-              </Text>
-            </View>
-            <View style={{ marginLeft: 30 }}>
-              <Text>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book.
-              </Text>
-            </View>
-          </View>
-          <View style={{ flexBasis: 1, flexGrow: 1, display: 'flex' }}>
-            <View>
-              <Text
+                <Text style={{ fontSize: 30 }}>{t('name')}</Text>
+                <Text style={{ fontSize: 10 }}>Frontend Engineer</Text>
+                <Text style={{ fontSize: 10 }}>indicozy.vercel.app</Text>
+              </View>
+              <View
                 style={{
-                  transform: 'rotate(90deg) translate(160px, 130px)',
-                  fontSize: 12
+                  flexBasis: 1,
+                  flexGrow: 1
                 }}
               >
-                EDUCATION
-              </Text>
+                <Text style={{ fontSize: 10 }}>
+                  I fall in love with products and design thoughtful user
+                  experiences that piece together the big picture with simple,
+                  impactful and shippable solutions focused on the customer.
+                </Text>
+              </View>
             </View>
-            <View style={{ marginLeft: 30 }}>
-              <Text>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book.
-              </Text>
+            <View
+              style={{ display: 'flex', flexDirection: 'row', marginTop: 20 }}
+            >
+              <View
+                style={{
+                  flexBasis: 1,
+                  flexGrow: 1,
+                  display: 'flex',
+                  marginRight: 10
+                }}
+              >
+                <ViewSection sidebar='education'>
+                  <TextSection>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </TextSection>
+                </ViewSection>
+              </View>
+
+              <View
+                style={{
+                  flexBasis: 1,
+                  flexGrow: 1,
+                  display: 'flex',
+                  marginRight: 10
+                }}
+              >
+                <ViewSection sidebar='hard skills'>
+                  <TextSection>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </TextSection>
+                </ViewSection>
+              </View>
             </View>
           </View>
         </View>
