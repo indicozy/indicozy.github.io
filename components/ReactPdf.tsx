5e705ee1559fca4b6ecf8928530503198e204155
indicozy <berkutkarlibai@gmail.com>
2023-02-23 20:51:21 +0600

    temporarily removed group channels

--- components/ReactPdf.tsx
+++ components/ReactPdf.tsx
@@ -47,17 +47,16 @@ const ViewSection: FC<{ children: ReactNode; sidebar: string }> = ({
       <View>
         <Text
           style={{
-            transform: 'rotate(-90deg) translate(50px, -120px)',
+            transform: 'rotate(-90deg) translate(-130px, -121px)',
             fontSize: 7,
             // fontFamily: 'TT Runs',
             textTransform: 'uppercase',
             // letterSpacing: 1.1,
-            fontWeight: 600
+            fontWeight: 600,
+            textAlign: 'right'
           }}
         >
-          {sidebar}
-          {'    '}
-          ---------
+          {sidebar} {'    '} ---------
         </Text>
       </View>
       <View
@@ -75,9 +74,13 @@ const ViewSection: FC<{ children: ReactNode; sidebar: string }> = ({
 }
 
 const TextSection: FC<{ children: ReactNode }> = ({ children }) => {
-  return <Text style={{ fontSize: 8, lineHeight: 1.2 }}>{children}</Text>
+  return <Text style={{ fontSize: 8, lineHeight: 1.15 }}>{children}</Text>
 }
-const Br: FC = () => <Text style={{ fontSize: 8 }}>------------------</Text>
+const Br: FC = () => (
+  <Text style={{ fontSize: 8, marginBottom: 2, marginTop: 2 }}>
+    ------------------
+  </Text>
+)
 
 // Create Document Component
 export const MyDocument = ({ t }) => {
@@ -142,7 +145,7 @@ export const MyDocument = ({ t }) => {
                   Frontend Engineer
                 </Text>
                 <Text style={{ fontSize: 8 }}>
-                  indicozy@gmail.com | github: indicozy
+                  indicozy@gmail.com | GitHub: indicozy
                 </Text>
                 <Text style={{ fontSize: 8, textDecoration: 'underline' }}>
                   indicozy.vercel.app
@@ -156,8 +159,8 @@ export const MyDocument = ({ t }) => {
               >
                 <Text style={{ fontSize: 10 }}>
                   I am passionate about creating exceptional user experiences by
-                  working with design teams to develop simple, impactful, and
-                  shippable solutions focused on the customer.
+                  working with team to develop simple, impactful, and shippable
+                  solutions focused on the customer.
                 </Text>
               </View>
             </View>
@@ -173,51 +176,64 @@ export const MyDocument = ({ t }) => {
                 }}
               >
                 <ViewSection sidebar='projects'>
-                  <TextSection>{`BeLearn (op-onai.kz, 02.2023 -)
+                  <TextSection>{`BeLearn (02.2023 - Present)
+                  op-onai.kz
+
                   Social Network for Active Learners with Personal Recommendation System.
                   Typescript, tRPC, Next, React, neo4j, Algolia, Vercel`}</TextSection>
                   <Br />
-                  <TextSection>{`Op-onai (old.op-onai.kz, 09.2022 - 01.2023)
+                  <TextSection>{`Op-onai (September 2022 - January 2023)
+                  old.op-onai.kz
+
                   Live Collaborative Learning for IT Schools.
                   Typescript, tRPC, Next, React, prismaORM, MongoDB, zustand, socket.io, Mantine, LevelDB`}</TextSection>
                   <Br />
-                  <TextSection>{`Personal Blog (indicozy.vercel.app, 01.2022 - )
+                  <TextSection>{`Personal Blog (January 2022 - Present)
+                  indicozy.vercel.app
+
                   My personal space for notes, activity and blog.
                   NotionCMS, Next, React, Tailwind, Vercel, Typescript`}</TextSection>
                   <Br />
-                  <TextSection>{`TinderBot (github.com/indicozy/cbtb, 08.2022 - 09.2022)
-                  Kaspi Market and Tinder for University.
+                  <TextSection>{`TinderBot (August 2022 - September 2022)
+                  github.com/indicozy/cbtb
+
+                  Ebay-like Marketplace and Tinder for University.
                   800 users and 80,000KZT in sales in 1 week`}</TextSection>
                   <Br />
-                  <TextSection>{`WMTM (github.com/indicozy/wmtm, 01.2021 - 02.2021 )
+                  <TextSection>{`WMTM (January 2021 - February 2021 )
+                    github.com/indicozy/wmtm
+
                   WM-agnostic Theme Manager for Linux in Bash.`}</TextSection>
                 </ViewSection>
                 <ViewSection sidebar='experience'>
                   <TextSection>
-                    {`Jezgrads Foundation
-                    Contributed to Vercel, Prisma, Pupilfirst, DiceBear, Searchkit, Vanilla-OS, uiwjs, Mantine, Faker-JS`}
+                    {`Jezgrads Foundation (November 2019 - Present)
+                      Head of Digitization and Design
+
+                      Raised 78 million tenge (~$180,000) for public funding, gathered 500 people into 6 events, collected information of every participant, developed website, WhatsApp bot, designed logo, instagram posts and merch.`}
                   </TextSection>
                   <Br />
-                  <TextSection>{`ACM CodeW (codew.kz, January 2022 - 02.2022)
-                  Website for Code-Women, ACM Student Chapter.
-                  Next, tRPC, PostgreSQL, AWS S3, ThreeJS, react-hook-form, Framer Motion, HeadlessUI, Tailwind`}</TextSection>
+                  <TextSection>{`ACM CodeW (January 2022 - January 2022)
+                  Frontend Developer, codew.kz
+
+                  Designed and developed website for Code-Women, ACM Student Chapter. Gathered 200 leads for event.`}</TextSection>
                   <Br />
-                  <TextSection>{`Mentorship (codew.kz, January 2022 - 02.2022)
-                  Website for Code-Women, ACM Student Chapter.
-                  Next, tRPC, PostgreSQL, AWS S3, ThreeJS, react-hook-form, Framer Motion, HeadlessUI, Tailwind`}</TextSection>
-                  <Br />
-                  <TextSection>{`Youtube Channel (codew.kz, January 2022 - 02.2022)
-                  Website for Code-Women, ACM Student Chapter.
-                  Next, tRPC, PostgreSQL, AWS S3, ThreeJS, react-hook-form, Framer Motion, HeadlessUI, Tailwind`}</TextSection>
+                  <TextSection>{`Mentorship (March 2021 - Present)
+                  Personal Mentor
+
+                  Personally mentored about 50 students on programming basics, frontend, backend and devops.`}</TextSection>
                   <Br />
                   <TextSection>
-                    {`QWANT
-                    Contributed to Vercel, Prisma, Pupilfirst, DiceBear, Searchkit, Vanilla-OS, uiwjs, Mantine, Faker-JS`}
+                    {`QWANT (May 2021 - December 2021)
+                    Marketing and Sales Manager
+
+                    As being responsible for all B2C sales, I have marketed and sold courses for 300 paying customers in 4 months. I developed telegram bot for marketing and sales funnel, which gathered 50 clients, developed website for sales, created design system for instagram posts, mentored web development and linux administration. `}
                   </TextSection>
                   <Br />
                   <TextSection>
-                    {`Open-Source
-                    Contributed to Vercel, Prisma, Pupilfirst, DiceBear, Searchkit, Vanilla-OS, uiwjs, Mantine, Faker-JS`}
+                    {`Open-Source (December 2021 - Present)
+
+                    Contributed to Vercel, Prisma, Pupilfirst, DiceBear, Searchkit, Vanilla-OS, uiwjs, Mantine and Faker-JS.`}
                   </TextSection>
                 </ViewSection>
               </View>
@@ -235,16 +251,16 @@ export const MyDocument = ({ t }) => {
                     {`FRONTEND (Advanced):
                     React, Next, Typescript, Solid, Remix, Vite, Svelte,
                     TailwindCSS, HeadlessUI, Framer Motion, Zustand,
-                    Redux, Jotai, ReactQuery, Axios, Storybook
+                    Redux, Jotai, ReactQuery, Axios, Storybook, Vue
 
                     BACKEND (Intermediate):
-                    Python, Go, NodeJS, Elixir, Rust, PrismaORM, Flask,
-                    Django, ExpressJS, Rails, Sinatra, Gin, Fiber, Git
+                    Python, Go, NodeJS, Elixir, Ruby, Rust, PrismaORM,
+                    Flask, Django, ExpressJS, Rails, Sinatra, Gin, Fiber
 
-                    DEVOPS (Advanced):
-                    Linux, Open/FreeBSD, Nix/NixOS, Terraform, Bash, Ansible, Docker, CI/CD, Vercel, Grafana, Zabbix
+                    DEVOPS (Intermediate):
+                    Linux, Nix/NixOS, Terraform, Bash, Ansible, Docker, CI/CD, Vercel, Grafana, Zabbix, AWS, Jenkins, Git
 
-                    DATABASES & API (Advanced):
+                    DATABASES & API (Intermediate):
                     neo4j, LevelDB, MongoDB, PostgreSQL, Cassandra, ScyllaDB, Redis, ElasticSearch, tRPC, REST, GraphQL
 
                     LOW/HIGH FIDELITY DESIGN (Intermediate):
@@ -252,28 +268,43 @@ export const MyDocument = ({ t }) => {
                     Miro, Photoshop, Illustrator, Figma, Inkscape
 
                     User/Customer Experience (Intermediate):
-                    User Research, Market Research, Usability Testing`}
+                    User Research, Market Research, Usability Testing
+
+                    PRODUCT/PROJECT MANAGEMENT (Beginner)
+                    Design Sprints, User Story Mapping, Release Planning, Agile
+                    Tools: Miro, Jira, Trello, Asana, Notion
+                    `}
                   </TextSection>
                 </ViewSection>
                 <ViewSection sidebar='soft skills'>
                   <TextSection>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
+                    {`The experience I have gained with the various roles I have had throughout my career has given me a very well-rounded range of soft skills. As a PM, I have led cross-functional teams of 10+ designers and engineers for a number of years. I have also worked as both a designer and engineer and have the ability to empathise with both disciplines as well as understand the more technical side of product delivery.
+
+                    -- Shape the vision and work towards a business or product
+- strategy while keeping solutions simple enough to ship quickly and iteratively
+- Use and understand data to back up my design solutions
+- Collaborate well with others cross-functionally
+- Know when to use low or high fidelity design methods
+- Share my work early and often and communicate effectively
+- Stay focused on the customer as the highest priority over my own opinions
+- Lead teams when needed and help to align teams on solutions or mission`}
                   </TextSection>
                 </ViewSection>
                 <ViewSection sidebar='education'>
                   <TextSection>
-                    {`NAZARBAYEV UNIVERSITY
+                    {`NAZARBAYEV UNIVERSITY (August 2018 - May 2023)
                     BA Degree in Sociology
-                   August 2018 - May 2023`}
 
-                    {`
-                    
-                    JEZKAZGAN KAZAKH-TURKISH LYCEUM
-                   May 2018`}
+                    Human-Computer Interaction
+                    Collaborative learning`}
+                  </TextSection>
+                  <Br />
+                  <TextSection>
+                    {`JEZKAZGAN KAZAKH-TURKISH LYCEUM
+                    May 2018
+
+                    Photography & Cinematography
+                    C/C++, Python, Automation`}
                   </TextSection>
                 </ViewSection>
               </View>
